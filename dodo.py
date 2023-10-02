042f5f1399766b4c206be9545aea4e7f1eb62331
Dennis N. Schneider <d.schneider.ulm@gmx.de>
2023-10-02 21:48:42 +0200

    fix: Incorrect dataset creation

--- dodo.py
+++ dodo.py
@@ -1,22 +1,13 @@
-DOIT_CONFIG = {
-    'action_string_formatting': 'new'
-}
+import os
 
 from doit.tools import Interactive
-import os
 from pathlib import Path
-import shutil
 
 
-def task_install():
-    """Install the environment."""
-    return {
-        "file_dep": ["requirements.txt"],
-        "targets": [".venv"],
-        "actions": [
-            '.make/install',
-        ]
-    }
+DOIT_CONFIG = {
+    'action_string_formatting': 'new',
+    'default_tasks': os.listdir("data") + ["configs"]
+}
 
 
 def task_datasets():
@@ -24,14 +15,50 @@ def task_datasets():
     Process all datasets.
     """
     root_dir = Path("data")
+    if not root_dir.exists():
+        root_dir.mkdir()
+        print("Initially created datasets directory.")
     datasets = filter(lambda d: d.is_dir(), root_dir.iterdir())
     for dataset_dir in datasets:
-        yield _process_dataset(dataset_dir)
-    return {
-        "basename": "datasets",
-        "file_dep": ["data"],
-        "actions": []
-    }
+        yield {
+            "name": f"create_preprocess_{dataset_dir.basename}",
+            "actions": ["cp .make/preprocess.py %s" % dataset_dir],
+            "uptodate": [(dataset_dir / "preprocess.py").exists()],
+        }
+        yield {
+            "name": f"create_fetch_{dataset_dir.basename}",
+            "actions": ["echo -n > %s/get_original_data.sh" % dataset_dir],
+            "uptodate": [(dataset_dir / "get_original_data.sh").exists()],
+        }
+        yield {
+            "name": f"mkd_preprocess_{dataset_dir.basename}",
+            "actions": ["mkdir %s/original" % dataset_dir],
+            "uptodate": [(dataset_dir / "original").exists()],
+        }
+        yield {
+            "name": f"mkd_original_{dataset_dir.basename}",
+            "actions": ["mkdir %s/preprocessed" % dataset_dir],
+            "uptodate": [(dataset_dir / "preprocessed").exists()],
+        }
+        yield {
+            "name": f"exec_fetch_{dataset_dir.basename}",
+            "file_dep": [dataset_dir / "get_original_data.sh"],
+            "uptodate": [False],
+            "actions": ["bash {dependencies}"],
+        }
+        for orig_file in Path.iterdir(dataset_dir / "original"):
+            yield {
+                "name": f"exec_preprocess_{dataset_dir}_{orig_file}",
+                "uptodate": [True],
+                "file_dep": [
+                    dataset_dir / "original" / orig_file / "graph_data.csv"
+                ],
+                "targets": [dataset_dir / "preprocessed" / (orig_file + ".pt")],
+                "actions": [
+                    dataset_dir + "/preprocess.py {dependencies} {targets}",
+                ],
+                "verbosity": 2,
+            }
 
 
 def task_configs():
@@ -41,10 +68,10 @@ def task_configs():
 
     python_files = _list_files("src", ignore_files, ignore_dirs)
     return {
-        "uptodate": [len(python_files) == 0],
+        # "uptodate": [len(python_files) == 0],
         "file_dep": list(python_files),
         "actions": [
-            'python make_configs.py -f {dependencies}'
+            'python make_configs.py -f {changed}'
         ],
         "verbosity": 2,
     }
@@ -56,52 +83,6 @@ def task_purge():
         "uptodate": [len(os.listdir("outputs")) == 0],
         "actions": [
             Interactive('git clean -ix outputs/*')
-        ],
-    }
-
-
-def _process_dataset(dataset_dir):
-    """
-    Process an entire dataset.
-    Create the file-structure if needed (original- and preprocessed directories).
-    Process each original file which has no corresponding preprocessed file.
-    """
-    original_dir = dataset_dir / "original"
-    preprocessed_dir = dataset_dir / "preprocessed"
-    if not original_dir.exists():
-        original_dir.mkdir(exist_ok=True)
-        preprocessed_dir.mkdir(exist_ok=True)
-        (dataset_dir / "get_original_data.sh").touch(exist_ok=True)
-        shutil.copy(".make/preprocess.py", str(dataset_dir))
-    # If get_original_data.sh has been changed, run it to retrieve original data.
-    yield {
-        "doc": f"Fetch original data for {dataset_dir.name}.",
-        "basename": dataset_dir.name,
-        "name": "Fetch",
-        "file_dep": [dataset_dir / "get_original_data.sh"],
-        "actions": [
-            'sh {dependencies}'
-        ]
-    }
-    for original_file in original_dir.iterdir():
-        corresponding_prep_file = preprocessed_dir / original_file.name
-        yield _preprocess_file(original_file, corresponding_prep_file)
-            
-
-def _preprocess_file(original_file, prep_file):
-    """
-    Pipe a single original file through the preprocessing-script and save it in
-    a corresponding preprocessed file.
-    """
-    dataset_path = original_file.parent.parent
-    yield {
-        "doc": "Preprocess the dataset %s." % dataset_path.name,
-        "basename": dataset_path.name,
-        "name": "Preprocess",
-        "file_dep": [original_file],
-        "targets": [prep_file],
-        "actions": [
-            'python %s {dependencies} {targets}' % (dataset_path / "preprocess.py")
         ],
     }
 
